01e49c2f5ebf84b1b01bd17c2b358416460ce937
Motorolpe <mauriciochulau@gmail.com>
2026-01-27 23:38:45 -0500

    Navbar editado
    -Color de fondo cambiado a uno más claro
    -Se escondieron los botones de Egresos y Filtros al ser admin (Esas funcionalidades son solo para usuarios)

--- src/components/Navegacion.jsx
+++ src/components/Navegacion.jsx
@@ -17,21 +17,25 @@ function Navegacion() {
         localStorage.setItem("esAdmin", "false")
     }
 
-    return <nav className="bg-blue-900 shadow-lg py-4">
+    return <nav className="bg-blue-500 shadow-lg py-4">
         <div className="max-w-7xl mx-auto px-8 flex justify-between items-center">
-            <img className="h-12" src="/imagenes/Palisade_Logo2.jpeg" alt="Logo" />
+            <img className="h-12 rounded-xl" src="/imagenes/Palisade_Logo2.jpeg" alt="Logo" />
             <div className="flex items-center gap-4">
                 {esAdmin && (
                     <Link to="/adminmain" className="text-white font-medium px-4 py-2 rounded-full hover:bg-yellow-300 hover:text-black">
                         Admin
                     </Link>
                 )}
+                {!esAdmin &&
                 <Link to="/usermain" className="text-white font-medium px-4 py-2 rounded-full hover:bg-yellow-300 hover:text-black">
                     Egresos
                 </Link>
+                }
+                {!esAdmin &&
                 <Link to="/usermainfiltros" className="text-white font-medium px-4 py-2 rounded-full hover:bg-yellow-300 hover:text-black">
                     Filtros
                 </Link>
+                }
                 <Link to="/" onClick={handleLogout} className="bg-red-500 text-white px-4 py-2 rounded-full font-medium hover:bg-red-600">
                     Cerrar Sesión
                 </Link>
