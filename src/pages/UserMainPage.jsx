ec682ae357eee3936fa11ee0a85e54878baa2e88
Motorolpe <mauriciochulau@gmail.com>
2026-01-28 15:46:53 -0500

    Correción import
    -Se eliminó un import duplicado que no permitía correr la página

--- src/pages/UserMainPage.jsx
+++ src/pages/UserMainPage.jsx
@@ -1,7 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import ListadoEgresos from "../components/ListadoEgresos"
 import Navegacion from "../components/Navegacion"
-import { useNavigate } from "react-router-dom";
 import PresupuestoCategoria from "../components/PresupuestoCategoria";
 
 /*
