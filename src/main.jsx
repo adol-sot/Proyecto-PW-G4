429158ab72e910d633caf260a22d71993f2c9154
adol-sot <adolfomartinsg06@gmail.com>
2026-01-28 01:01:03 -0500

    Grafico filtrado de egresos
    Avance del grafico de egresos, en caso de usarlo y no funcionar, instalar:
    npm install chart.js react-chartjs-2

--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,17 @@ import CambiarContrasena from './pages/CambiarContrasena'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import UserMainPage from './pages/UserMainPage'
 import AdminMainPage from './pages/AdminMainPage'
+import FiltrarGraficoEgreso from './pages/FiltrarGraficoEgreso'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<LoginPage />}/>
-        <Route path='/cambiarcontra' element={<CambiarContrasena />}/>
+        <Route path='/' element={ <LoginPage />}/>
+        <Route path='/cambiarcontra' element={ <CambiarContrasena />}/>
         <Route path='/usermain' element={ <UserMainPage />}/>
         <Route path='/adminmain' element={ <AdminMainPage />}/>
+        <Route path='/grafico-egresos' element={ <FiltrarGraficoEgreso />}/>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
