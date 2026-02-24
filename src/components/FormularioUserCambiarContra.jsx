db41e906d3f056c65a14bfe10b0c0533b02cc075
Motorolpe <mauriciochulau@gmail.com>
2026-02-23 23:48:47 -0500

    Cambio de contraseña implementado usuario autorizado
    -Se implementó el cambio de contraseño cuando un usuario autorizado que se acuerde su contraseña quiera cambiarla

--- src/components/FormularioUserCambiarContra.jsx
+++ src/components/FormularioUserCambiarContra.jsx
@@ -1,7 +1,41 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react"
 
 function FormularioUserCambiarContra() {
     const navigate = useNavigate()
+
+    const [password, setPassword] = useState("")
+    const [newPassword, setNewPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+
+    async function cambiarPassword() {
+
+        if (newPassword !== confirmPassword) {
+            console.log("Las contraseñas no coinciden")
+            return;
+        }
+
+        const response = await fetch("http://localhost:8000/usuarios/cambiar-password-autorizado", {
+            method: "PUT",
+            body: JSON.stringify({
+                email: localStorage.getItem("MAIL"),
+                old_password: password,
+                new_password: newPassword
+            }), 
+            headers: {
+                "x-token": localStorage.getItem("TOKEN"),
+                "content-type" : "application/json"
+            }
+        })
+
+        const data = await response.json()
+
+        if (response.ok) {
+            console.log("Contraseña actualizada correctamente")
+        } else {
+            console.log(data.detail)
+        }
+     }
 
     async function CambiarContra() {
         const correo = localStorage.getItem("MAIL")
@@ -31,22 +65,29 @@ function FormularioUserCambiarContra() {
                 <p className="my-6">Ingresa tu contraseña actual y tu nueva contraseña. Una vez completado te enviaremos un correo para confirmar la acción.</p>
                 <form className="grid">
                     <label className="text-sm font-medium mb-1">Contraseña actual</label>
-                    <input type="password" className="rounded-full bg-gray-200 px-4 py-1"/>
+                    <input type="password" className="rounded-full bg-gray-200 px-4 py-1"
+                    value={password}
+                    onChange={function(e){setPassword(e.target.value)}}/>
                     <p className="text-sm mt-1 text-blue-900 hover:text-amber-600" 
                     onClick={ function(){ CambiarContra() } }> ¿Olvidaste tu contraseña? </p>
 
                     <label className="text-sm font-medium mb-1 mt-5">Nueva contraseña</label>
-                    <input type="password" className="rounded-full bg-gray-200 px-4 py-1"/>
+                    <input type="password" className="rounded-full bg-gray-200 px-4 py-1"
+                    value={newPassword}
+                    onChange={function(e){setNewPassword(e.target.value)}}/>
 
                     <label className="text-sm font-medium mb-1 mt-3">Confirmar contraseña</label>
-                    <input type="password" className="rounded-full bg-gray-200 px-4 py-1"/>
+                    <input type="password" className="rounded-full bg-gray-200 px-4 py-1"
+                    value={confirmPassword}
+                    onChange={function(e){setConfirmPassword(e.target.value)}}/>
                 </form>
 
                 <form className="flex justify-between items-center mt-6">
                     <button onClick={function(){navigate("/usermainseguridad")}} type="button" className="bg-gray-300 px-6 py-2 rounded-full hover:bg-gray-400">
                         Cancelar
                     </button>
-                    <button onClick={function(){navigate("/usermainseguridad")}} type="button" className="bg-yellow-300 px-6 py-2 rounded-full font-semibold text-black hover:bg-amber-600 hover:text-white">
+
+                    <button onClick={ function(){ cambiarPassword() } } type="button" className="bg-yellow-300 px-6 py-2 rounded-full font-semibold text-black hover:bg-amber-600 hover:text-white">
                         Confirmar
                     </button>
                 </form>
