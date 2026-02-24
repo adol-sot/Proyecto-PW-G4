e1045028c0fa2e43c8d7ef93bcc09f39bc40eb8c
Motorolpe <mauriciochulau@gmail.com>
2026-02-24 16:17:04 -0500

    Popup Login
    -Se implementó popup si se producen errores durante el login inicial

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,21 @@
 import { Link, useNavigate } from "react-router-dom"
+import { useState } from "react";
 import FormularioLogin from "../components/FormularioLogin"
 import TituloLogin from "../components/TituloLogin"
 
 function LoginPage() {
 
     const navigate = useNavigate()
+
+    const [modalVisible, setModalVisible] = useState(false)
+    const [modalMensaje, setModalMensaje] = useState("")
+    const [esExito, setEsExito] = useState(false)
+
+    function mostrarModal(mensaje, exito = false) {
+        setModalMensaje(mensaje)
+        setEsExito(exito)
+        setModalVisible(true)
+    }
 
     async function loginHTTP(correo, contra) {
         const resp = await fetch("http://127.0.0.1:8000/login", {
@@ -39,18 +50,21 @@ function LoginPage() {
     }
 
     async function login(correo, contra) {
-        const resultadologin = await loginHTTP(correo, contra)
-
-         if ( resultadologin ) {
-            const role = localStorage.getItem("USER_ROLE")
+        try {
+            const resultadologin = await loginHTTP(correo, contra)
+            if ( resultadologin ) {
+                const role = localStorage.getItem("USER_ROLE")
             if (role == "admin") {
                 navigate("/adminmain")
             }
             if (role == "user") {
                 navigate("/usermain")
             }
-        } else {
-            console.log("Credenciales Incorrectas")
+            } else {
+                mostrarModal("Credenciales Incorrectas")
+            }
+        } catch (error) {
+            alert("Error de conexión con el servidor")
         }
 
         /*
@@ -67,12 +81,27 @@ function LoginPage() {
         */
     }
 
+    function cerrarModal() {
+        setModalVisible(false)
+    }
+
     return <div className="flex justify-center bg-blue-900 shadow-lg min-h-screen">
         <div className="border-2 rounded-4xl border-gray-300 shadow-md p-10 my-20 bg-white xl:w-1/3 lg:w-1/3 md:w-1/2 sm:w-1/2"> 
             <img  className="w-2/3 place-self-center" src="/imagenes/Palisade_Logo2.jpeg"/> 
             <TituloLogin />
             <FormularioLogin onLogin={ login }/>
         </div>
+        {modalVisible && (
+                <div className="fixed inset-0 bg-black/50 flex items-center justify-center">
+                    <div className="bg-white rounded-xl p-6 w-80 text-center shadow-xl">
+                        <p className="mb-4">{modalMensaje}</p>
+                        <button onClick={cerrarModal}
+                            className="px-4 py-2 rounded-full font-semibold bg-blue-500 text-white hover:bg-blue-600">
+                            Aceptar
+                        </button>
+                    </div>
+                </div>
+        )}
     </div>
 }
 
